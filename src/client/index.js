e1fcde46a2117e9878e2d1a1f3561bad88f55e5e
Sakaowduan <billmichelle33@gmail.com>
2021-06-24 21:01:16 +1000

    fixing functionality

--- src/client/index.js
+++ src/client/index.js
@@ -7,5 +7,7 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
-alert("I EXIST")
-console.log("CHANGE!!");
+export {
+    checkForName,
+    handleSubmit
+}
