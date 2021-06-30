63dc8cc15a9b711c8a41564070d5b2c2884871ae
Sakaowduan <billmichelle33@gmail.com>
2021-06-30 21:26:50 +1000

    fetch meaningcloud api

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,55 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+var path = require('path');
+// Middleware
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fetch = require('node-fetch');
 
-const app = express()
+// Start up instance of app
+const app = express();
+const port = process.env.PORT || 8081;
 
-app.use(express.static('dist'))
+// MeaningCloudAPI
+dotenv.config();
+const mockAPIResponse = require('./mockAPI.js');
+const mainAPI = 'https://api.meaningcloud.com/sentiment-2.1?key=';
+const apikey = process.env.API_KEY;
+const language = '&lang=auto';
+const urlInput = '&url=';
 
+// Use middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('dist'));
 console.log(__dirname)
 
+// root send file to the browser
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+app.listen(port, function () {
+    console.log(`App listening on port ${port}`)
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+app.post('/information', getInfomation);
+async function getInfomation(req, res) {
+    console.log(`Information: ${req.body}`);
+    const url = mainAPI + apikey + language + urlInput + req.body;
+    const sentiment_response = await fetch(url);
+
+    try {
+        const data = await sentiment_response.json();
+        res.send(data);
+    } catch(err){
+        console.error(err.stack);
+    }    
+}
