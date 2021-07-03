f4e85d5907fbf028f69522f6573abcebec2c6f14
Sakaowduan <billmichelle33@gmail.com>
2021-07-04 08:52:21 +1000

    minor changes on server

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,7 +4,7 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let {urlDataForm} = document.getElementById('inputURL').value
+    let urlDataForm = document.getElementById('inputURL').value
     Client.validURL(urlDataForm)
 
     console.log("::: Form Submitted :::")
@@ -21,10 +21,10 @@ function handleSubmit(event) {
     .then((res) => res.json())
     .then(function(res) {
         console.log('Data from the MC-API', res);
-        document.getElementById('results').innerHTML = res.agreement;
-        document.getElementById('subjectivity').innerHTML = res.subjectivity;
+        document.getElementById('results').innerHTML = 'Agreement:' + res.agreement;
+        document.getElementById('subjectivity').innerHTML = 'Subjectivity: ' + res.subjectivity;
         document.getElementById('scores').innerHTML = `Polarity: ${scores(res.score_tag)}`;
-        document.getElementById('confidence').innerHTML = res.confidence;
+        document.getElementById('confidence').innerHTML = 'Confidence: ' +res.confidence;
     })
 
     function scores(text){
